3119857a5228f074a38e821fe51e7ca8a0877424
CristianRM05 <crm00423@gmail.com>
2024-03-02 18:28:17 +0100

    agregado comentarios a la clase AudioPlayer

--- JAVA-ONEPIECE-GAME/OnePiece/src/audio/AudioPlayer.java
+++ JAVA-ONEPIECE-GAME/OnePiece/src/audio/AudioPlayer.java
@@ -12,6 +12,13 @@ import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
+/**
+ * Esta clase representa un reproductor de audio que gestiona la reproducción de música de fondo y efectos de sonido en un juego.
+ * Permite cargar archivos de audio, ajustar el volumen, reproducir canciones y efectos, así como controlar el estado de reproducción.
+ * @author Giovanni/Cristian
+ * @version 1.0
+ */
+
 public class AudioPlayer {
 
 	public static int MENU_1 = 0;
@@ -38,23 +45,34 @@ public class AudioPlayer {
 		playSong(MENU_1);
 	}
 
+	/**
+	 * Carga las canciones de fondo del juego.
+	 */
 	private void loadSongs() {
 		String[] names = { "menu", "level1", "level2" };
 		songs = new Clip[names.length];
 		for (int i = 0; i < songs.length; i++)
 			songs[i] = getClip(names[i]);
 	}
-
+	/**
+	 * Carga los efectos de sonido del juego.
+	 */
 	private void loadEffects() {
 		String[] effectNames = { "die", "jump", "gameover", "lvlcompleted", "attack1", "attack2", "attack3" };
 		effects = new Clip[effectNames.length];
 		for (int i = 0; i < effects.length; i++)
 			effects[i] = getClip(effectNames[i]);
-		
+
 		updateEffectsVolume();
-		
+
 	}
 
+	/**
+	 * Obtiene un Clip de audio a partir del nombre del archivo.
+	 *
+	 * @param name El nombre del archivo de audio.
+	 * @return Un objeto Clip que representa el archivo de audio.
+	 */
 	private Clip getClip(String name) {
 		URL url = getClass().getResource("/cargarAudio/" + name + ".wav");
 		AudioInputStream audio;
@@ -64,50 +82,71 @@ public class AudioPlayer {
 			Clip c = AudioSystem.getClip();
 			c.open(audio);
 			return c;
-
 		} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
 
 			e.printStackTrace();
 		}
-
 		return null;
 
 	}
-
+	/**
+	 * Establece el volumen general del reproductor de audio.
+	 *
+	 * @param volume El nivel de volumen deseado (0 a 1).
+	 */
 	public void setVolume(float volume) {
 		this.volume = volume;
 		updateSongVolume();
 		updateEffectsVolume();
 	}
-
+	/**
+	 * Detiene la reproducción de la canción actual.
+	 */
 	public void stopSong() {
 		if (songs[currentSongId].isActive())
 			songs[currentSongId].stop();
 	}
-
+	/**
+	 * Selecciona la canción de fondo según el índice de nivel proporcionado.
+	 *
+	 * @param lvlIndex El índice del nivel actual.
+	 */
 	public void setLevelSong(int lvlIndex) {
 		if (lvlIndex % 2 == 0)
 			playSong(LEVEL_1);
 		else
 			playSong(LEVEL_2);
 	}
-
+	/**
+	 * Método llamado cuando se completa un nivel.
+	 * Detiene la reproducción de la canción actual y reproduce el efecto de nivel completado.
+	 */
 	public void lvlCompleted() {
 		stopSong();
 		playEffect(LVL_COMPLETED);
 	}
-
+	/**
+	 * Reproduce un sonido de ataque aleatorio.
+	 */
 	public void playAttackSound() {
 		int start = 4;
 		start += rand.nextInt(3);
 		playEffect(start);
 	}
-
+	/**
+	 * Reproduce un efecto de sonido específico.
+	 *
+	 * @param effect El índice del efecto de sonido a reproducir.
+	 */
 	public void playEffect(int effect) {
 		effects[effect].setMicrosecondPosition(0);
 		effects[effect].start();
 	}
-
+	/**
+	 * Reproduce una canción de fondo específica.
+	 *
+	 * @param song El índice de la canción de fondo a reproducir.
+	 */
 	public void playSong(int song) {
 		stopSong();
 
@@ -116,7 +155,9 @@ public class AudioPlayer {
 		songs[currentSongId].setMicrosecondPosition(0);
 		songs[currentSongId].loop(Clip.LOOP_CONTINUOUSLY);
 	}
-
+	/**
+	 * Alterna el estado de silencio de las canciones de fondo.
+	 */
 	public void toggleSongMute() {
 		this.songMute = !songMute;
 		for (Clip c : songs) {
@@ -124,7 +165,9 @@ public class AudioPlayer {
 			booleanControl.setValue(songMute);
 		}
 	}
-
+	/**
+	 * Alterna el estado de silencio de los efectos de sonido y reproduce un efecto de salto si el silencio se desactiva.
+	 */
 	public void toggleEffectMute() {
 		this.effectMute = !effectMute;
 		for (Clip c : effects) {
@@ -134,7 +177,9 @@ public class AudioPlayer {
 		if (!effectMute)
 			playEffect(JUMP);
 	}
-
+	/**
+	 * Actualiza el volumen de la canción actual según el volumen general.
+	 */
 	private void updateSongVolume() {
 
 		FloatControl gainControl = (FloatControl) songs[currentSongId].getControl(FloatControl.Type.MASTER_GAIN);
@@ -143,7 +188,9 @@ public class AudioPlayer {
 		gainControl.setValue(gain);
 
 	}
-
+	/**
+	 * Actualiza el volumen de todos los efectos de sonido según el volumen general.
+	 */
 	private void updateEffectsVolume() {
 		for (Clip c : effects) {
 			FloatControl gainControl = (FloatControl) c.getControl(FloatControl.Type.MASTER_GAIN);
